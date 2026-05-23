063373b51e0f12c453f262c29fd5c73377c55c62
monazarea <monazarea3@gmail.com>
2026-05-23 04:43:30 -0400

    added lab3

--- RxSwiftLec1/TableViewController.swift
+++ RxSwiftLec1/TableViewController.swift
@@ -6,16 +6,130 @@
 //
 
 import UIKit
+import RxSwift
+
+struct Person{
+    let firstName : String
+    let lastName : String
+    
+    init(fullName: String){
+        let parts = fullName.split(separator: " ")
+        self.firstName = String(parts.first ?? "")
+        self.lastName = parts.count > 1 ? parts.dropFirst().joined(separator: " ") : ""
+    }
+    
+    var displayString : String {
+        return self.lastName.isEmpty ? firstName : "\(firstName) \(lastName)"
+    }
+}
+
 
 class TableViewController: UIViewController {
 
+    @IBOutlet weak var mytableview: UITableView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //setupTableUsingNormalWay()
+       // setupTableUsingNormalWay2()
+        //setupTableUsingNormalWayUsingGlobal()
+        setupTableViewWithPublishSubject()
     }
     
 
+    func setupTableUsingNormalWay(){
+        let initialData = ["beshoy", "Mona Ahmed", "Ahmed Essam"].map { Person(fullName: $0) }
+        let expandedData = initialData + [Person(fullName: "Ali Gamal")]
+        
+        let observable1 = Observable<[Person]>.create{ observar in
+            observar.onNext(initialData)
+            observar.onCompleted()
+            return Disposables.create()
+        }
+        let observable2 = Observable.just(expandedData).delay(.seconds(2), scheduler: MainScheduler.instance)
+        
+        let combinedObservable = Observable.concat([observable1, observable2])
+        combinedObservable.bind(to: mytableview.rx.items(cellIdentifier: "Cell", cellType: NameTableViewCell.self)){ index , Person , cell in
+            cell.textLabel?.text = Person.displayString
+        }.disposed(by: disposeBag)
+    }
+    
+    func setupTableUsingNormalWay2(){
+        let initialData = ["beshoy", "Mona Ahmed", "Ahmed Essam"].map { Person(fullName: $0) }
+        let expandedData = initialData + [Person(fullName: "Ali Gamal")]
+        
+        let observable = Observable<[Person]>.create{ observar in
+            observar.onNext(initialData)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                observar.onNext(expandedData)
+                observar.onCompleted()
+
+            })
+            return Disposables.create()
+        }
+        
+        
+        observable.bind(to: mytableview.rx.items(cellIdentifier: "Cell", cellType: NameTableViewCell.self)){ index , Person , cell in
+            cell.textLabel?.text = Person.displayString
+        }.disposed(by: disposeBag)
+    }
+    
+    
+    func setupTableUsingNormalWayUsingGlobal(){
+        let initialData = ["beshoy", "Mona Ahmed", "Ahmed Essam"].map { Person(fullName: $0) }
+        let expandedData = initialData + [Person(fullName: "Ali Gamal")]
+        
+        let bgSchdular = ConcurrentDispatchQueueScheduler(qos: .background)
+        let observable = Observable<[Person]>.create{ observar in
+            observar.onNext(initialData)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                observar.onNext(expandedData)
+                observar.onCompleted()
+
+            })
+            return Disposables.create()
+        }
+        observable.subscribeOn(bgSchdular)
+            .asDriver(onErrorJustReturn: [])
+            .drive(mytableview.rx.items(cellIdentifier: "Cell", cellType: NameTableViewCell.self)){
+                index, person, cell in
+                cell.textLabel?.text = person.displayString
+            }.disposed(by: disposeBag)
+        
+    }
+    
+    var currentData: [Person] = []
+    let dataSubject = PublishSubject<[Person]>()
+
+    func setupTableViewWithPublishSubject() {
+        mytableview.isEditing = true
+        
+        dataSubject
+            .bind(to: mytableview.rx.items(cellIdentifier: "Cell")) { index, person, cell in
+                cell.textLabel?.text = person.displayString
+            }
+            .disposed(by: disposeBag)
+        
+        currentData = ["beshoy", "Mona Ahmed", "Ahmed Essam"].map { Person(fullName: $0) }
+        dataSubject.onNext(currentData)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.currentData.append(Person(fullName: "Ali Gamal"))
+            self.dataSubject.onNext(self.currentData)
+        }
+        
+        mytableview.rx.itemDeleted
+            .subscribe(onNext: { [weak self] indexPath in
+                guard let self = self else { return }
+                
+                self.currentData.remove(at: indexPath.row)
+                
+                self.dataSubject.onNext(self.currentData)
+            })
+            .disposed(by: disposeBag)
+    }
     /*
     // MARK: - Navigation
 
--- RxSwiftLec1/ViewController.swift
+++ RxSwiftLec1/ViewController.swift
@@ -38,33 +38,81 @@ class ViewController: UIViewController {
 //            }.disposed(by: disposeBag)
    
         
-        let names = ["mona","esraa"]
-        let observable1 : Observable<[String]> = Observable<[String]>.just(names)
+//        let names = ["mona","esraa"]
+//        let observable1 : Observable<[String]> = Observable<[String]>.just(names)
+//        
+//        let observable2 : Observable<[String]> = Observable<[String]>.of(names,names)
+//        let observable3 : Observable<String> = Observable<String>.from(names)
+//
+//        let observable4 : Observable<String> = Observable<String>.create{ observer in          observer.onNext("sahar")
+//            observer.onNext("abeer")
+//            DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+//                observer.onNext("amal")
+//                observer.onCompleted()
+//            })
+//            
+//            observer.onNext("menna")
+//            return Disposables.create()
+//        }
+//        
+//        observable4.subscribe(onNext: { value in
+//            print(value)
+//        },onCompleted: {
+//          print("Completed")
+//        },onDisposed: {
+//            print("disposed..")
+//        })
         
-        let observable2 : Observable<[String]> = Observable<[String]>.of(names,names)
-        let observable3 : Observable<String> = Observable<String>.from(names)
-
-        let observable4 : Observable<String> = Observable<String>.create{ observer in          observer.onNext("sahar")
-            observer.onNext("abeer")
-            DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-                observer.onNext("amal")
-                observer.onCompleted()
-            })
-            
-            observer.onNext("menna")
+        
+        //testCombineLatest()
+       // testCombineLatestUsingCreate()
+        testReplySubject()
+    }
+    
+    func testCombineLatest(){
+        let observable1 = Observable<Int>.from([1, 2, 3, 4])
+        let observable2 = Observable<Int>.from([5,6,7,8])
+        Observable.combineLatest(observable1, observable2).subscribe{
+            value in
+            print(value)
+        }
+    }
+    func testCombineLatestUsingCreate(){
+        let observable1 = Observable<Int>.create{observer in
+            observer.onNext(1)
+            observer.onNext(2)
+            observer.onNext(3)
+            observer.onNext(4)
+            observer.onCompleted()
             return Disposables.create()
         }
         
-        observable4.subscribe(onNext: { value in
+        let observable2 = Observable<Int>.create{observer in
+            observer.onNext(5)
+            observer.onNext(6)
+            observer.onNext(7)
+            observer.onNext(8)
+            observer.onCompleted()
+            return Disposables.create()
+        }
+        Observable.combineLatest(observable1, observable2).subscribe{
+            value in
             print(value)
-        },onCompleted: {
-          print("Completed")
-        },onDisposed: {
-            print("disposed..")
-        })
+        }
     }
     
-    
+    func testReplySubject(){
+        let replySubject = ReplaySubject<Int>.create(bufferSize: 2 )
+        replySubject.onNext(1)
+        replySubject.onNext(2)
+        replySubject.onNext(3)
+        replySubject.subscribe{value in
+            print(value)
+        }
+        replySubject.onNext(4)
+        replySubject.onNext(5)
+
+    }
 
     
     
