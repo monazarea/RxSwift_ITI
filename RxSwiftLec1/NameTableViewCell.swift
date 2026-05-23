9d7e27ee6ff8e20e818b501b2a1a523ba2ac1c2a
monazarea <monazarea3@gmail.com>
2026-05-23 04:38:44 -0400

    Lab3

---
+++ RxSwiftLec1/NameTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NameTableViewCell.swift
+//  RxSwiftLec1
+//
+//  Created by Mona Zarea on 20/05/2026.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
